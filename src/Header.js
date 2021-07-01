06925b8d6c4f4176b2022413672d5b2114c43bc2
Sokos6 <will.sokolowski@gmail.com>
2021-06-30 19:20:51 -0500

    changes to app and header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+
+  console.log("We are in the header")
+  return (
+    <>
+    <h1>THIS IS OUR HEADER</h1>
+    <p>This is a p tag</p>
+    </>
+  )
+}
+
+export default Header;
